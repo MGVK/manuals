9fdeb7a82c50252dae6a35c72ed9e4b5c94c2b64
MGVK <MukyLike@yandex.ru>
2019-01-06 12:04:29 +0300

    Update java8_oracle_install.sh
    raspbian version

--- java8_oracle_install.sh
+++ java8_oracle_install.sh
@@ -3,6 +3,10 @@
 #if add-apt-repository not installed:
 #sudo apt install software-properties-common
 
+#for raspbian
+#sudo apt install oracle-java8-jdk
+
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
 sudo apt-get install oracle-java8-installer
+
