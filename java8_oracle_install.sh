11c9f3f49e48a506a35cf60f6944270734541f87
MGVK <MukyLike@yandex.ru>
2019-01-06 12:01:29 +0300

    Update java8_oracle_install.sh
    add options

--- java8_oracle_install.sh
+++ java8_oracle_install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#if add-apt-repository not installed:
+#sudo apt install software-properties-common
+
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
 sudo apt-get install oracle-java8-installer
