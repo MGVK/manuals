774e7e31e4bdc40eb293efdaf83e1f6e12164fc7
MGVK <MukyLike@yandex.ru>
2019-03-27 15:10:37 +0300

    small fixes

--- install_ovpn.sh
+++ install_ovpn.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt install -y liblzo2-dev libpam0g-dev
+sudo apt install -y liblzo2-dev libpam0g-dev unzip
 
 wget https://swupdate.openvpn.org/community/releases/openvpn-2.4.7.zip
 
@@ -38,12 +38,12 @@ PrivateTmp=true
 KillMode=mixed
 Type=forking
 ExecStartPre= /bin/bash -c 'if [ ! -d /run/openvpn ]; then  mkdir /run/openvpn; fi'
-ExecStart=/usr/local/sbin/openvpn --daemon ovpn-%i --status /run/openvpn/%i.status 10 --cd /etc/openvp$
+ExecStart=/usr/local/sbin/openvpn --daemon ovpn-%i --status /run/openvpn/%i.status 10 --cd /etc/openvpn --script-security 2 --config /etc/openvpn/%i.conf --writepid /run/openvpn/%i.pid
 PIDFile=/run/openvpn/%i.pid
 ExecReload=/bin/kill -HUP $MAINPID
 WorkingDirectory=/etc/openvpn
 ProtectSystem=yes
-CapabilityBoundingSet=CAP_IPC_LOCK CAP_NET_ADMIN CAP_NET_BIND_SERVICE CAP_NET_RAW CAP_SETGID CAP_SETUI$
+CapabilityBoundingSet=CAP_IPC_LOCK CAP_NET_ADMIN CAP_NET_BIND_SERVICE CAP_NET_RAW CAP_SETGID CAP_SETUID CAP_SYS_CHROOT CAP_DAC_READ_SEARCH CAP_AUDIT_WRITE
 LimitNPROC=10
 DeviceAllow=/dev/null rw
 DeviceAllow=/dev/net/tun rw
@@ -53,6 +53,6 @@ WantedBy=multi-user.target " > openvpn@.service
 
 #uncomment this if you want to work with systemctl
 
-#sudo cp openvpn.service /lib/systemd/system/openvpn.service
-#sudo cp openvpn.service /lib/systemd/system/openvpn@.service
+sudo cp openvpn.service /lib/systemd/system/openvpn.service
+sudo cp openvpn.service /lib/systemd/system/openvpn@.service
 
