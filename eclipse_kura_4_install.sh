ec18aa91653a347bf26fce22cd6771592621a014
MGVK <MukyLike@yandex.ru>
2019-05-18 15:48:37 +0300

    Update eclipse_kura_4_install.sh
    some fixes

--- eclipse_kura_4_install.sh
+++ eclipse_kura_4_install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #install dependencies
-sudo apt install isc-dhcp-server dos2unix bind9 ethtool telnet bluez-hcidump hostapd unzip iw wireless-tools
-
+sudo apt install -y isc-dhcp-server dos2unix bind9 ethtool telnet bluez-hcidump hostapd unzip iw wireless-tools
+java -version && sudo apt install -y openjdk-8-*
 
 cd /tmp
 wget http://ftp.fau.de/eclipse/kura/releases/4.0.0/kura_4.0.0_raspberry-pi-2-3_installer.deb
-dpkg -i kura_4.0.0_raspberry-pi-2-3_installer.deb
+sudo dpkg -i kura_4.0.0_raspberry-pi-2-3_installer.deb
 
